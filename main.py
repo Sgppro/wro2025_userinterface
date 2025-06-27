b27a420fca0e8a1a0cd76f0d21653fa04205105d
SGPPRO <dickson.wong.20090730@gmail.com>
2025-06-27 17:09:51 +0800

    first edition

---
+++ main.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template, redirect
+
+app = Flask(__name__)
+
+@app.route("/")
+def rootpage():
+    return redirect("/main.html")
+
+@app.route("/main.html")
+def mainPage():
+    return render_template("main.html")
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0",port=5500)
